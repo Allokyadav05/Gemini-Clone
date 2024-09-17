1120edf60a0c1d6442116585f93c9514bf2f2d0c
Allokyadav05 <allokyadav05@gmail>
2024-09-17 14:49:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    {/* <div className='w-[100vw] h-[20vh] text-white bg-slate-950'>chl ja</div> */}
+    </>
+  )
+}
+
+export default App;
